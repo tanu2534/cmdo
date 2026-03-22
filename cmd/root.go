65dc8b338405c6d061b6dff162182783d6b20812
tanu <tanuintrovert4321@hmail.com>
2026-03-22 22:11:24 +0530

    Build: Bump version to 1.0.6

--- cmd/root.go
+++ cmd/root.go
@@ -9,7 +9,7 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:     "cmdo",
-	Version: "1.0.5",
+	Version: "1.0.6",
 	Short:   "cmdo is a command logger tool",
 	Long:    "cmdo is a command logger tool for tracking and managing command line history.",
 	Run: func(cmd *cobra.Command, args []string) {
